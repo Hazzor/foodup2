b420ab7a9c81bf6a96bd428cbd7af8fba777cb92
Hazzor <hesterliong@gmail.com>
2017-03-05 09:59:13 +0800

    latest

--- src/pages/food/food.ts
+++ src/pages/food/food.ts
@@ -6,12 +6,16 @@ import { FoodOrderPage } from '../food-order/food-order';
 
 import { SellerInfosPage } from '../seller-infos/seller-infos';
 
+import { AboutPage } from '../about/about';
+
+
 @Component({
   selector: 'page-food',
   templateUrl: 'food.html'
 })
 export class FoodPage {
  food
+//  postMessage : boolean = false;
   constructor(public navCtrl: NavController, private modalPage:ModalController) {
     this.food = "Lunch"
   }
@@ -23,6 +27,14 @@ export class FoodPage {
 
   openInfo(){
     this.navCtrl.push(SellerInfosPage);
+  }
+
+  postStatus(){
+    // let postStatus = this.modalPage.create(AboutPage);
+    // postStatus.present();
+    this.navCtrl.push(AboutPage);
+    // this.postMessage = true;
+
   }
 
 }
